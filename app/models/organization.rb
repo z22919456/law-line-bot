f9d30bde255168eff732575e62ae75e1330f5a31
z01241 <z01241@lawbroker.com.tw>
2022-04-15 21:35:48 +0800

    today relationship

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,4 +2,6 @@ class Organization < ApplicationRecord
   has_many :users
   has_many :daily_reports
   has_many :org_summeries
+  has_one :today_org_summery, -> { where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day) },
+          class_name: 'OrgSummery'
 end
